72fe75201133e9d38fc44dd5a9ab06c5a89b9e23
Joseph Lewis <joelewis3335@yahoo.com>
2018-07-24 20:24:38 -0700

    rm balls

--- balls.py
+++
@@ -1,9 +0,0 @@
-print('TESTING, 1, 2, 3. Mic Check. Is this thing on?')
-
-<<<<<<< HEAD
-print("If you're reading this it's too late")
-=======
-def get_big():
-    return 'too small, get much bigger'
-    
->>>>>>> 505b4587a5a1862c729d1a2509c96c308acfb5ae
