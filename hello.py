a4302b59287e04bab46ead3d8ceb9419b80dbffc
Alaaddin Alokby <alokby.alaaddin@gmail.com>
2018-07-24 21:04:38 -0700

    added templates directory, index.html and user.html for page home and user home

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)   # Create application instance as a flask object
 
 @app.route('/')
 def index():
-        return '<h1>Workout Tracker</h1>'
+        return render
 
 @app.route('/user/<name>')
 def user(name):
-        return '<h1>Hello, {}!</h1>'.format(name)
+        return render_template('index.html')    # looks for file in templates dir
+
+@app.route('/agent')
+def get_agent():
+        user_agent = request.headers.get('User-Agent')
+        return '<p>Your browser is {}!</p>'.format(user_agent)
