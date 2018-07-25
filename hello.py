a20002c95333ff1040c063eeff551ea5eeed667a
Alaaddin Alokby <alokby.alaaddin@gmail.com>
2018-07-24 22:19:06 -0700

    trying to help joe show off

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)   # Create application instance as a flask object
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
