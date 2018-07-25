8cf6393070be0139a47501a0cead206e5295fcee
Alaaddin Alokby <alokby.alaaddin@gmail.com>
2018-07-24 20:49:14 -0700

    added app.route

--- hello.py
+++ hello.py
@@ -5,3 +5,7 @@ app = Flask(__name__)   # Create application instance as a flask object
 @app.route('/')
 def index():
         return '<h1>Workout Tracker</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+        return '<h1>Hello, {}!</h1>'.format(name)
